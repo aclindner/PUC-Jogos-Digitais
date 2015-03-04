537ff0e5225eea4f54d58afb4dc80063e508d0ee
unknown <allison.lindner@LTCH0314.cwbdis.pucpr.br>
2015-03-04 20:08:56 -0300

    Criando Classe Galinha

---
+++ Oficina 03/Aula 01 - Intro/Galinha.cpp
@@ -0,0 +1,11 @@
+#include "Galinha.h"
+
+Galinha::Galinha() {
+	SetNome("Galinha Pintadinha");
+	SetSom("Po pô PÓ");
+	SetPeso(5);
+}
+
+Galinha::Galinha() {
+
+}
---
+++ Oficina 03/Aula 01 - Intro/Galinha.h
@@ -0,0 +1,13 @@
+using namespace std;
+
+#include "Animal.h"
+
+class Galinha : private Animal{
+
+public:
+	Galinha();
+	~Galinha();
+
+private:
+
+};
