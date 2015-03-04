55f7cdf10ab716cd8766f5b1bb518a46da367d00
unknown <allison.lindner@LTCH0314.cwbdis.pucpr.br>
2015-03-04 20:23:56 -0300

    Criando Main

---
+++ Oficina 03/Aula 01 - Intro/Source.cpp
@@ -0,0 +1,14 @@
+#include "Cao.h"
+#include "Galinha.h"
+#include "Animal.h"
+
+int main() {
+
+	Animal arara;
+
+	arara.SetNome("Arara Rára");
+	arara.SetSom("Aaaahhh aaaaaaaahh");
+	arara.SetPeso(400);
+
+	return 0;
+}
