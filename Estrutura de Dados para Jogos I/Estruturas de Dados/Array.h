02e849176f0bc13cc1816afe94fff94de03c45b1
unknown <allison.lindner@LCET0314.cwbdis.pucpr.br>
2015-03-09 21:30:01 -0300

    Implementação do método Resize()

--- Estrutura de Dados para Jogos I/Estruturas de Dados/Array.h
+++ Estrutura de Dados para Jogos I/Estruturas de Dados/Array.h
@@ -9,21 +9,51 @@ public:
 	T* m_array;
 	int m_size;
 	
-	Array(int p_size)
+	Array( int p_size )
 	{
-		m_array = new T[p_size];
+
+		m_array = new T[ p_size ];
 		m_size = p_size;
-	}
+
+	}//Fim Construtor
 
 	~Array()
 	{
+
 		if (m_rray != 0)
 		{
 			delete[] m_array;
 		}
 
-		m_array = 0;
-	}
+		m_array = 0; //NULL
+
+	}//Fim Destrutor
+
+	void Resize( int p_size )
+	{
+
+		T* new_array = new T[ p_size ];
+		int min;
+
+		if ( new_array == 0 )
+			return;
+
+		if ( p_size < m_size )
+			min = p_size;
+		else
+			min = m_size;
+
+		for ( int i = 0; i < min; i++ )
+			new_array[ i ] = m_array[ i ];
+
+		m_size = psize;
+
+		if ( m_array != 0 )
+			delete[] m_array;
+
+		m_array = new_array;
+
+	}//Fim Resize( int p_size )
 
 
 private:
