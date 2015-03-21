365c23c596831b1162754bdaf3059d2b0a87cd11
Allison Lindner <lindner.allison@gmail.com>
2015-03-20 21:49:35 -0300

    Criando janela em SDL

--- Oficina 03/SDL - First Run/Main.cpp
+++ Oficina 03/SDL - First Run/Main.cpp
@@ -3,5 +3,15 @@
 int main(int argc, char** argv) {
 
 	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window *_window;
+	_window = SDL_CreateWindow("First Game",
+								SDL_WINDOWPOS_CENTERED,
+								SDL_WINDOWPOS_CENTERED,
+								800, 600,
+								SDL_WINDOW_SHOWN);
+
+
+	system("PAUSE");
 	return 0;
 }
