2394131dd87affa5895d97b80d40ccfa7933bea4
Allison Lindner <lindner.allison@gmail.com>
2015-03-23 21:29:10 -0300

    Testando a SLinkedList

--- Estrutura de Dados para Jogos I/Estruturas de Dados/Source.cpp
+++ Estrutura de Dados para Jogos I/Estruturas de Dados/Source.cpp
@@ -4,29 +4,40 @@
 
 using namespace std;
 
-void MostrarArrayInteiro(int* p_array, int tam) {
-
-	for (int i = 0; i < tam; i++)
-		cout << "[" << i << "] = " << p_array[i] << endl;
-
+template<class T>
+void showFromBegin(SListNode<T> * p_node) {
+	SListNode<T> * itr = p_node;
+	while (itr != 0)
+	{
+		std::cout << itr->m_data << std::endl;
+		itr = itr->m_next;
+	}
 }
 
 int main()
 {
-	//Teste Array
-	Array<int> meuarray(5);
-	meuarray[0] = 99;
-	meuarray[1] = 88;
-	meuarray[2] = 77;
-	meuarray[3] = 66;
-	meuarray[4] = 55;
+	SListNode<int> * node;
+	node = new SListNode<int>();
+	node->m_data = 10;
 
-	MostrarArrayInteiro(meuarray, 5);
+	node->m_next = new SListNode<int>();
+	node->m_next->m_data = 20;
 
-	//---------------------------------------------
+	node->m_next->m_next = new SListNode<int>();
+	node->m_next->m_next->m_data = 30;
 
+	//Do começo ao Fim
+	showFromBegin(node);
+
+	SListNode<char> * node2;
+	node2 = new SListNode<char>();
+	node2->m_data = 'a';
+	node2->InsertAfter('c');
+	node2->InsertAfter('b');
 	
-
+	//Do começo ao Fim
+	showFromBegin(node2);
+	
 	system("PAUSE");
 	return 0;
 }
